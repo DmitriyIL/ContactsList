366dd0755685073238f01ed3e89e71fe5c013f71
DmitriyIL <idmitriy.mgdn@gmail.com>
2018-11-14 10:04:19 +0300

    Turn back separated from MainActivity JsonParserTask; Fixed bags with JsonParserTask execution;

--- app/src/main/java/com/lashchenov/contactsListApp/activity/MainActivity.java
+++ app/src/main/java/com/lashchenov/contactsListApp/activity/MainActivity.java
@@ -2,7 +2,6 @@ package com.lashchenov.contactsListApp.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,18 +11,15 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.lashchenov.contactsListApp.R;
-import com.lashchenov.contactsListApp.UsersJsonParser;
 import com.lashchenov.contactsListApp.adapter.UsersAdapter;
 import com.lashchenov.contactsListApp.data.Data;
 import com.lashchenov.contactsListApp.data.DataSQLiteImpl;
+import com.lashchenov.contactsListApp.task.JsonParserTask;
 import com.lashchenov.contactsListApp.pojo.User;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 
@@ -31,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView usersView;
     private UsersAdapter usersAdapter;
     private Toolbar toolbar;
-    private View progressBarView;
 
     private Data data;
+    private JsonParserTask jsonParserTask;
 
 
     @Override
@@ -42,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         data = new DataSQLiteImpl(this);
-        progressBarView = this.findViewById(R.id.progressBar);
 
         initToolbar();
         initRecyclerView();
@@ -60,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.round_cache) {
-            new JsonParserTask().execute();
+            new JsonParserTask(this).execute();
             return true;
         }
         return false;
@@ -105,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
     private void fillRecyclerView() {
         List<User> userList;
         if (data.isEmpty()) {
-            new JsonParserTask().execute();
+            new JsonParserTask(this).execute();
         } else {
             //get data from cache
             userList = data.getUsers();
@@ -116,38 +111,6 @@ public class MainActivity extends AppCompatActivity {
 
     public UsersAdapter getUsersAdapter() {
         return usersAdapter;
-    }
-
-
-    private class JsonParserTask extends AsyncTask<Void, Void, List<User>> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressBarView.setVisibility(View.VISIBLE);
-            usersAdapter.clearItems();
-        }
-
-        @Override
-        protected List<User> doInBackground(Void... params) {
-            UsersJsonParser usersJsonParser = new UsersJsonParser();
-            return usersJsonParser.parseUsers();
-        }
-
-
-        @Override
-        protected void onPostExecute(List<User> users) {
-            super.onPostExecute(users);
-            Collections.sort(users, new Comparator<User>() {
-                @Override
-                public int compare(User o1, User o2) {
-                    return Integer.compare(o1.getId(), o2.getId());
-                }
-            });
-            progressBarView.setVisibility(View.INVISIBLE);
-            Toast.makeText(MainActivity.this, "Users loaded", Toast.LENGTH_SHORT).show();
-            usersAdapter.setItems(users);
-            data.setUsers(users);
-        }
     }
 }
 
